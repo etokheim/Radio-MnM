95e8dee500393635e8547e1987345003910a988a
Erling Tokheim <e@tokheim.no>
2020-01-26 22:21:46 +0100

    Feature: Plays radio streams

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import datetime
 from RPLCD.gpio import CharLCD
 from RPi import GPIO
 import time
+import vlc
 
 GPIO.setmode(GPIO.BCM)
 
@@ -16,9 +17,37 @@ lcd = CharLCD(cols=16,
               charmap = 'A02'
 )
 
+lcd.clear()
+
 lcd.write_string('Radio M&M')
 
 time.sleep(1.5)
-
 lcd.clear()
-GPIO.cleanup()
+
+lcd.write_string('Fetching audio streams')
+
+time.sleep(1)
+lcd.clear()
+
+lcd.write_string('Got 3 streams')
+
+time.sleep(1)
+lcd.clear()
+
+
+playingChannel = 0
+channels = ["http://lyd.nrk.no/nrk_radio_p1_ostlandssendingen_mp3_m", "http://lyd.nrk.no/nrk_radio_alltid_nyheter_mp3_m", "http://lyd.nrk.no/nrk_radio_jazz_mp3_m"]
+channelNames = ["NRK P1", "NRK P2", "NRK P3"]
+
+lcd.write_string(channelNames[playingChannel])
+
+player = vlc.MediaPlayer(channels[playingChannel])
+player.play()
+
+# time.sleep(5)
+# player.stop()
+# player = vlc.MediaPlayer(channels[2])
+# player.play()
+
+#GPIO.cleanup()
+###############
