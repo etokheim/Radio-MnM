eeb7f0c4c4453fea4f59988f3081a3cdc8a9baf1
Erling Tokheim <e@tokheim.no>
2020-02-08 15:24:57 +0000

    Logging buton push time

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from RPLCD.gpio import CharLCD
 from RPi import GPIO
 import time
 import vlc
+from datetime import datetime
+import threading
 
 playingChannel = 0
 channels = ["http://lyd.nrk.no/nrk_radio_p1_ostlandssendingen_mp3_m", "http://lyd.nrk.no/nrk_radio_alltid_nyheter_mp3_m", "http://lyd.nrk.no/nrk_radio_jazz_mp3_m"]
@@ -31,6 +33,33 @@ def nextChannel():
 
     print("Channel " + str(playingChannel) + " (" + channels[playingChannel] + ")")
     lcd.write_string(channelNames[playingChannel])
+
+
+# Non-blocking interval execution
+import threading
+
+class ThreadJob(threading.Thread):
+    def __init__(self,callback,event,interval):
+        '''runs the callback function after interval seconds
+
+        :param callback:  callback function to invoke
+        :param event: external event for controlling the update operation
+        :param interval: time in seconds after which are required to fire the callback
+        :type callback: function
+        :type interval: int
+        '''
+        self.callback = callback
+        self.event = event
+        self.interval = interval
+        super(ThreadJob,self).__init__()
+
+    def run(self):
+        while not self.event.wait(self.interval):
+            self.callback()
+
+
+
+event = threading.Event()
 
 
 GPIO.setmode(GPIO.BCM)
@@ -78,8 +107,45 @@ player.play()
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+pushing = False
+pushStart = 0
+
+
+def logButtonState():
+    print(pushing)
+
+# interval = ThreadJob(logButtonState,event,0.5)
+# interval.start()
+
+def buttonUp(pushTime):
+    print("Button up. Held for " + str(pushTime))
+
+def buttonDown():
+    print("Button down")
+
+def buttonClick():
+    print("Button click")
+
 while True:
+    global pushing, pushStart
+
     input_state = GPIO.input(18)
-    if input_state == False:
-        nextChannel()
-        time.sleep(0.2)
+
+    if input_state == True and pushing == True:
+        pushing = False
+
+    # If pushing
+    if input_state == False and pushStart == 0:
+        # nextChannel()
+        # time.sleep(0.2)
+        
+        pushStart = int(round(time.time() * 1000))
+        pushing = True
+        buttonDown()
+
+    elif pushStart != 0 and pushing == False:
+        now = int(round(time.time() * 1000))
+        holdTime = now - pushStart
+        # print("Held the button for " + str(holdTime) + " (" + str(now) + " - " + str(pushStart) + ")")
+        buttonUp(holdTime)
+        pushStart = 0
