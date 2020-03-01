38e13688e56b5ea9f402503fd401fdf2036be678
Erling Tokheim <e@tokheim.no>
2020-03-01 08:36:48 +0000

    Using attachable events to handle button events
    Events like button down, up, click and long press

--- app.py
+++ app.py
@@ -5,35 +5,16 @@ import time
 import vlc
 from datetime import datetime
 import threading
+import zope.event
+
+# GPIO.cleanup()
 
 playingChannel = 0
 channels = ["http://lyd.nrk.no/nrk_radio_p1_ostlandssendingen_mp3_m", "http://lyd.nrk.no/nrk_radio_alltid_nyheter_mp3_m", "http://lyd.nrk.no/nrk_radio_jazz_mp3_m"]
 channelNames = ["NRK P1", "NRK P2", "NRK P3"]
-
-
-# Skips to the next channel. If you are on the last channel
-# skip to the first instead.
-def nextChannel():
-    # Using global variables outside of necessity is usually frowned upon by Python developers
-    global playingChannel, channels, player
-
-    lcd.clear()
-
-    if playingChannel + 1 > len(channels) - 1:
-        playingChannel = 0
-
-    else:
-        playingChannel = playingChannel + 1
-
-    print("Next channel is" + str(playingChannel))
-
-    player.stop()
-    player = vlc.MediaPlayer(channels[playingChannel])
-    player.play()
-
-    print("Channel " + str(playingChannel) + " (" + channels[playingChannel] + ")")
-    lcd.write_string(channelNames[playingChannel])
-
+longClickThreshold = 1500
+button1DownStart = 0
+GPIO.setmode(GPIO.BCM)
 
 # Non-blocking interval execution
 import threading
@@ -62,7 +43,6 @@ class ThreadJob(threading.Thread):
 event = threading.Event()
 
 
-GPIO.setmode(GPIO.BCM)
 
 lcd = CharLCD(cols=16,
               rows=2,
@@ -97,18 +77,112 @@ lcd.write_string(channelNames[playingChannel])
 player = vlc.MediaPlayer(channels[playingChannel])
 player.play()
 
-# time.sleep(5)
-# player.stop()
-# player = vlc.MediaPlayer(channels[2])
-# player.play()
+#####
 
-#GPIO.cleanup()
-###############
+time.sleep(5)
+player.stop()
+player = vlc.MediaPlayer(channels[2])
+player.play()
 
+##############
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 pushing = False
 pushStart = 0
+
+
+
+# I really have no idea how Zope events works, but this project is a very "learn
+# as we go" project. Anyways, what I wanted to do here was to create an event system
+# where I could subscribe to several events: click, down, up, longClick. This I
+# figured out, but I'm still unable to pass arguments down to the event handlers.
+# But as I don't need to pass down arguments yet, It's not a big problem yet. An
+# example of arguments could be an event for clicking the mouse. That should resolve
+# in an event, click, with arguments like pointer position x and y.
+import zope.event.classhandler
+# import zope.event
+
+class button1Click(object):
+    def __repr__(self):
+        return self.__class__.__name__
+
+# Skips to the next channel. If you are on the last channel
+# skip to the first instead.
+# Event is set to the the event which calls it. In this function's case it should be
+# set to "click".
+def nextChannel(event):
+    print("nextChannel %r" % event)
+
+    # Using global variables outside of necessity is usually frowned upon by Python developers
+    global playingChannel, channels, player
+
+    lcd.clear()
+
+    if playingChannel + 1 > len(channels) - 1:
+        playingChannel = 0
+
+    else:
+        playingChannel = playingChannel + 1
+
+    print("Next channel is" + str(playingChannel))
+
+    player.stop()
+    player = vlc.MediaPlayer(channels[playingChannel])
+    player.play()
+
+    print("Channel " + str(playingChannel) + " (" + channels[playingChannel] + ")")
+    lcd.write_string(channelNames[playingChannel])
+
+
+
+class button1Down(object):
+    def __repr__(self):
+        return self.__class__.__name__
+
+def button1DownHandler(event):
+    global button1DownStart
+
+    button1DownStart = int(round(time.time() * 1000))
+
+    print("button1DownHandler %r" % event)
+
+
+
+
+class button1Up(object):
+    def __repr__(self):
+        return self.__class__.__name__
+
+def button1UpHandler(event):
+    global button1DownStart
+    
+    print("button1UpHandler %r" % event)
+
+    button1DownStart = 0
+
+
+
+
+# import zope.event.classhandler
+# import zope.event
+
+class button1LongPress(object):
+    def __repr__(self):
+        return self.__class__.__name__
+
+def button1LongPressHandler(event):
+    global longClickThreshold
+    
+    print("button1LongPressHandler %r" % event)
+
+
+zope.event.classhandler.handler(button1LongPress, button1LongPressHandler)
+zope.event.classhandler.handler(button1Up, button1UpHandler)
+zope.event.classhandler.handler(button1Down, button1DownHandler)
+zope.event.classhandler.handler(button1Click, nextChannel)
+
 
 
 def logButtonState():
@@ -116,15 +190,6 @@ def logButtonState():
 
 # interval = ThreadJob(logButtonState,event,0.5)
 # interval.start()
-
-def buttonUp(pushTime):
-    print("Button up. Held for " + str(pushTime))
-
-def buttonDown():
-    print("Button down")
-
-def buttonClick():
-    print("Button click")
 
 while True:
     global pushing, pushStart
@@ -136,16 +201,18 @@ while True:
 
     # If pushing
     if input_state == False and pushStart == 0:
-        # nextChannel()
-        # time.sleep(0.2)
-        
         pushStart = int(round(time.time() * 1000))
         pushing = True
-        buttonDown()
+        zope.event.notify(button1Down())
 
     elif pushStart != 0 and pushing == False:
         now = int(round(time.time() * 1000))
         holdTime = now - pushStart
+
+        zope.event.notify(button1Up())
         # print("Held the button for " + str(holdTime) + " (" + str(now) + " - " + str(pushStart) + ")")
-        buttonUp(holdTime)
+        if holdTime >= longClickThreshold:
+            zope.event.notify(button1LongPress())
+        else:
+            zope.event.notify(button1Click())
         pushStart = 0
