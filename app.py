1f7055a3cae4a1be61ed8ae23c95b85b0e91dc99
Erling Tokheim <e@tokheim.no>
2020-04-06 18:19:10 +0100

    Added config for stream quality (not in use yet)

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ import threading
 import zope.event
 # import requests
 
+# config
+
+# Qualities
+# Put a number in the quality variable, and the stream closest to that bitrate will be used.
+# 32 kbps - Poor audio quality
+# 48 kbps - A reasonable lower end rate for longer speech-only podcasts
+# 64 kbps - A common bitrate for speech podcasts.
+# 128 kbps - Common standard for musical and high quality podcasts.
+# 320 kbps - Very high quality - almost indistinguishable from a CD.
+quality = 128
 
 playingChannel = 0
 # Will be populated by an API request
