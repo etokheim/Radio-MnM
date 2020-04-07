db1522c6531d8c04179485f995d48e9adcb48617
Erling Tokheim <e@tokheim.no>
2020-04-07 15:54:42 +0100

    Now monitoring switch1 in its own loop

--- radio_mnm/app.py
+++ radio_mnm/app.py
@@ -52,30 +52,9 @@ zope.event.classhandler.handler(switches.switch1.longPress, longPressHandler)
 def run():
 	while True:
 		time.sleep(0.01)
-
-		button1State = GPIO.input(18)
+		
 		button2State = GPIO.input(17)
 
-		if button1State == True and switches.switch1.pushing == True:
-			switches.switch1.pushing = False
-
-		# If switches.switch1.pushing
-		if button1State == False and switches.switch1.pushStart == 0:
-			switches.switch1.pushStart = int(round(time.time() * 1000))
-			switches.switch1.pushing = True
-			zope.event.notify(switches.switch1.down())
-
-		elif switches.switch1.pushStart != 0 and switches.switch1.pushing == False:
-			now = int(round(time.time() * 1000))
-			holdTime = now - switches.switch1.pushStart
-
-			zope.event.notify(switches.switch1.up())
-			if holdTime >= config.longClickThreshold:
-				zope.event.notify(switches.switch1.longPress())
-			else:
-				zope.event.notify(switches.switch1.click())
-			switches.switch1.pushStart = 0
-		
 		# If pushing
 		if button2State == False:
 			if switches.power.pushing == False:
--- switches/switch1.py
+++ switches/switch1.py
@@ -2,6 +2,9 @@ from RPi import GPIO
 import time
 from controls import channels
 import zope.event.classhandler
+from threading import Thread
+from switches import switch1
+from config import config
 
 pushing = False
 pushStart = 0
@@ -28,3 +31,42 @@ class up(object):
 class longPress(object):
 	def __repr__(self):
 		return self.__class__.__name__
+
+
+class MonitorSwitch(Thread):
+	def __init__(self):
+		Thread.__init__(self)
+		self.running = True
+
+	def run(self):
+		while self.running:
+			time.sleep(0.01)
+
+			button1State = GPIO.input(18)
+
+			if button1State == True and switch1.pushing == True:
+				switch1.pushing = False
+
+			# If switch1.pushing
+			if button1State == False and switch1.pushStart == 0:
+				switch1.pushStart = int(round(time.time() * 1000))
+				switch1.pushing = True
+				zope.event.notify(switch1.down())
+
+			elif switch1.pushStart != 0 and switch1.pushing == False:
+				now = int(round(time.time() * 1000))
+				holdTime = now - switch1.pushStart
+
+				zope.event.notify(switch1.up())
+				if holdTime >= config.longClickThreshold:
+					zope.event.notify(switch1.longPress())
+				else:
+					zope.event.notify(switch1.click())
+				switch1.pushStart = 0
+
+	def stop(self):
+		self.running = False
+
+a = MonitorSwitch()
+
+a.start()
