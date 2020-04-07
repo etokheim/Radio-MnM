fb17cf55ec680a352ee758fd6c3d0c3663d1376c
Erling Tokheim <e@tokheim.no>
2020-04-07 15:34:40 +0100

    Moved more of the logic to app.py

--- radio_mnm/app.py
+++ radio_mnm/app.py
@@ -12,6 +12,43 @@ from switches import power
 from display import display
 from controls import channels
 
+# Event is set to the the event which calls it. In this function's case it should be
+# set to "click".
+def clickHandler(event):
+	print("click %r" % event)
+
+	channels.bump()
+
+zope.event.classhandler.handler(switches.switch1.click, clickHandler)
+
+
+def downHandler(event):
+	global downStart
+
+	downStart = int(round(time.time() * 1000))
+
+	print("downHandler %r" % event)
+
+zope.event.classhandler.handler(switches.switch1.down, downHandler)
+
+
+def upHandler(event):
+	global downStart
+	
+	print("upHandler %r" % event)
+
+	downStart = 0
+
+zope.event.classhandler.handler(switches.switch1.up, upHandler)
+
+
+def longPressHandler(event):
+	print("longPressHandler %r" % event)
+	channels.bump(-1)
+
+zope.event.classhandler.handler(switches.switch1.longPress, longPressHandler)
+
+
 def run():
 	while True:
 		time.sleep(0.01)
--- switches/switch1.py
+++ switches/switch1.py
@@ -16,45 +16,15 @@ class click(object):
 	def __repr__(self):
 		return self.__class__.__name__
 
-# Event is set to the the event which calls it. In this function's case it should be
-# set to "click".
-def clickHandler(event):
-	print("click %r" % event)
-
-	channels.bump()
-
 
 class down(object):
 	def __repr__(self):
 		return self.__class__.__name__
 
-def downHandler(event):
-	global downStart
-
-	downStart = int(round(time.time() * 1000))
-
-	print("downHandler %r" % event)
-
 class up(object):
 	def __repr__(self):
 		return self.__class__.__name__
 
-def upHandler(event):
-	global downStart
-	
-	print("upHandler %r" % event)
-
-	downStart = 0
-
 class longPress(object):
 	def __repr__(self):
 		return self.__class__.__name__
-
-def longPressHandler(event):
-	print("longPressHandler %r" % event)
-	channels.bump(-1)
-
-zope.event.classhandler.handler(longPress, longPressHandler)
-zope.event.classhandler.handler(up, upHandler)
-zope.event.classhandler.handler(down, downHandler)
-zope.event.classhandler.handler(click, clickHandler)
