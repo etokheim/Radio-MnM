4a762bb3b6605c7fbaf4af05cb25f1a75422003f
Erling Tokheim <e@tokheim.no>
2020-12-30 16:06:59 +0000

    Style: Removed the temperature logging to console

--- radio_mnm/components/temperatureAndHumidity.py
+++ radio_mnm/components/temperatureAndHumidity.py
@@ -29,7 +29,7 @@ class TemperatureAndHumidity(threading.Thread):
 				humidity = self.sensorDht.humidity
 
 				if humidity is not None and temperature is not None:
-					print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+					# print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
 					self.temperature = temperature
 					self.humidity = humidity
 					self.lastUpdateTime = int(time.time() * 1000)
