30400e6a106628a4e29c6c8640f72922a6978782
Erling Tokheim <e@tokheim.no>
2021-01-09 19:04:34 +0100

    Feat: Support getting dictionary as prop

--- radio_mnm/components/navigationRotary.py
+++ radio_mnm/components/navigationRotary.py
@@ -1,16 +1,19 @@
 from handlers import rotaryPolling
+from validator import validate
 import logging
 logger = logging.getLogger("Radio_mnm")
 
 class NavigationRotary():
-	def __init__(self, radio, clk, dt):
-		self.radio = radio
-		self.clk = clk
-		self.dt = dt
-
-		self.rotary = rotaryPolling.Rotary(self.clk, self.dt)
-
+	def __init__(self, radio, props):
+		# Check props
+		assert type(props["GPIO"]["clk"]) == int, "NavigationRotary's clk pin is not an int. Check your config.yml"
+		assert type(props["GPIO"]["data"]) == int, "NavigationRotary's data pin is not an int. Check your config.yml"
 		
+		self.radio = radio
+		self.clk = props["GPIO"]["clk"]
+		self.data = props["GPIO"]["data"]
+
+		self.rotary = rotaryPolling.Rotary(self.clk, self.data)
 		self.rotary.addEventListener("left", self.rotaryLeftHandler)
 		self.rotary.addEventListener("right", self.rotaryRightHandler)
 
