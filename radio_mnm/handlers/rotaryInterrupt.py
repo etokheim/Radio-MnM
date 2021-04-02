86055c41710f9e9d6c222f07268f8a931ba9c099
Erling Tokheim <e@tokheim.no>
2021-04-02 11:37:20 +0200

    Feat: Add asyncio to rotaryInterrupt

--- radio_mnm/handlers/rotaryInterrupt.py
+++ radio_mnm/handlers/rotaryInterrupt.py
@@ -1,6 +1,7 @@
 import logging
 logger = logging.getLogger("Radio_mnm")
 from RPi import GPIO
+import asyncio
 
 GPIO.setmode(GPIO.BCM)
 	
@@ -12,6 +13,8 @@ class Rotary():
 		self.left = []
 		self.right = []
 
+		self.loop = asyncio.get_event_loop()
+
 		# Set up GPIO pins
 		GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -20,23 +23,27 @@ class Rotary():
 		GPIO.add_event_detect(clk, GPIO.FALLING, callback=self.rotationHandler, bouncetime=80)
 
 	# Loops through the callbacks parameter (array) and executes them
-	def dispatch(self, callbacks):
+	async def dispatch(self, callbacks):
 		for callback in callbacks:
 			if callback:
-				callback()
+				if callback[1]:
+					args = callback[1]
+					callback[0](*args)
+				else:
+					callback[0]()
 
-	def addEventListener(self, type, callback):
+	def addEventListener(self, type, callback, args=[]):
 		if type == "left":
-			self.left.append(callback)
+			self.left.append([callback, args])
 		elif type == "right":
-			self.right.append(callback)
+			self.right.append([callback, args])
 		else:
 			raise Exception("Event type " + str(callback) + "is not supported.")
 
 	def rotationHandler(self, channel):
 		if GPIO.input(self.dt) == 1:
 			logger.debug("Rotary right (GPIO " + str(self.clk) + " and " + str(self.dt) + ")")
-			self.dispatch(self.right)
+			asyncio.run_coroutine_threadsafe(self.dispatch(self.right), self.loop)
 		else:
 			logger.debug("Rotary left (GPIO " + str(self.clk) + " and " + str(self.dt) + ")")
-			self.dispatch(self.left)
+			asyncio.run_coroutine_threadsafe(self.dispatch(self.left), self.loop)
